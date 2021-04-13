1754ef7fd5db2c73cc34ecc459f39c8c0dccd194
MagdalenaKumala <79920505+MagdalenaKumala@users.noreply.github.com>
2021-04-13 19:58:19 +0200

    Update drzewa_binarne.py

--- drzewa_binarne.py
+++ drzewa_binarne.py
@@ -1,72 +1,111 @@
-import pandas as pd # data processing
-import numpy as np # working with arrays
-import matplotlib.pyplot as plt # visualization
-from matplotlib import rcParams # figure size
-from termcolor import colored as cl # text customization
-
-from sklearn.tree import DecisionTreeClassifier as dtc # tree algorithm
-from sklearn.model_selection import train_test_split # splitting the data
-from sklearn.metrics import accuracy_score # model precision
-from sklearn.tree import plot_tree # tree diagram
-
-rcParams['figure.figsize'] = (25, 20)
-
-df = pd.read_csv('drzewko_dane.csv')
-df.drop('Unnamed: 0', axis = 1, inplace = True)
-
-print(cl(df.head(), attrs = ['bold']))
-
-df.info()
-
-for i in df.Sex.values:
-    if i  == 'M':
-        df.Sex.replace(i, 0, inplace = True)
-    else:
-        df.Sex.replace(i, 1, inplace = True)
-        
-for i in df.BP.values:
-    if i == 'LOW':
-        df.BP.replace(i, 0, inplace = True)
-    elif i == 'NORMAL':
-        df.BP.replace(i, 1, inplace = True)
-    elif i == 'HIGH':
-        df.BP.replace(i, 2, inplace = True)
-        
-for i in df.Cholesterol.values:
-    if i == 'LOW':
-        df.Cholesterol.replace(i, 0, inplace = True)
-    else:
-        df.Cholesterol.replace(i, 1, inplace = True)
-        
-print(cl(df, attrs = ['bold']))
-
-X_var = df[['Sex', 'BP', 'Age', 'Cholesterol', 'Na_to_K']].values # independent variable
-y_var = df['Drug'].values # dependent variable
-
-print(cl('X variable samples : {}'.format(X_var[:5]), attrs = ['bold']))
-print(cl('Y variable samples : {}'.format(y_var[:5]), attrs = ['bold']))
-
-X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.2, random_state = 0)
-
-print(cl('X_train shape : {}'.format(X_train.shape), attrs = ['bold'], color = 'red'))
-print(cl('X_test shape : {}'.format(X_test.shape), attrs = ['bold'], color = 'red'))
-print(cl('y_train shape : {}'.format(y_train.shape), attrs = ['bold'], color = 'green'))
-print(cl('y_test shape : {}'.format(y_test.shape), attrs = ['bold'], color = 'green'))
-
-model = dtc(criterion = 'entropy', max_depth = 4)
-model.fit(X_train, y_train)
-
-pred_model = model.predict(X_test)
-
-print(cl('Accuracy of the model is {:.0%}'.format(accuracy_score(y_test, pred_model)), attrs = ['bold']))
-
-feature_names = df.columns[:5]
-target_names = df['Drug'].unique().tolist()
-
-plot_tree(model, 
-          feature_names = feature_names, 
-          class_names = target_names, 
-          filled = True, 
-          rounded = True)
-
-plt.savefig('tree_visualization.png')
+# Run this program on your local python
+# interpreter, provided you have installed
+# the required libraries.
+  
+# Importing the required packages
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+  
+# Function importing Dataset
+def importdata():
+    balance_data = pd.read_csv(
+'https://archive.ics.uci.edu/ml/machine-learning-'+
+'databases/balance-scale/balance-scale.data',
+    sep= ',', header = None)
+      
+    # Printing the dataswet shape
+    print ("Dataset Length: ", len(balance_data))
+    print ("Dataset Shape: ", balance_data.shape)
+      
+    # Printing the dataset obseravtions
+    print ("Dataset: ",balance_data.head())
+    return balance_data
+  
+# Function to split the dataset
+def splitdataset(balance_data):
+  
+    # Separating the target variable
+    X = balance_data.values[:, 1:5]
+    Y = balance_data.values[:, 0]
+  
+    # Splitting the dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split( 
+    X, Y, test_size = 0.3, random_state = 100)
+      
+    return X, Y, X_train, X_test, y_train, y_test
+      
+# Function to perform training with giniIndex.
+def train_using_gini(X_train, X_test, y_train):
+  
+    # Creating the classifier object
+    clf_gini = DecisionTreeClassifier(criterion = "gini",
+            random_state = 100,max_depth=3, min_samples_leaf=5)
+  
+    # Performing training
+    clf_gini.fit(X_train, y_train)
+    return clf_gini
+      
+# Function to perform training with entropy.
+def tarin_using_entropy(X_train, X_test, y_train):
+  
+    # Decision tree with entropy
+    clf_entropy = DecisionTreeClassifier(
+            criterion = "entropy", random_state = 100,
+            max_depth = 3, min_samples_leaf = 5)
+  
+    # Performing training
+    clf_entropy.fit(X_train, y_train)
+    return clf_entropy
+  
+  
+# Function to make predictions
+def prediction(X_test, clf_object):
+  
+    # Predicton on test with giniIndex
+    y_pred = clf_object.predict(X_test)
+    print("Predicted values:")
+    print(y_pred)
+    return y_pred
+      
+# Function to calculate accuracy
+def cal_accuracy(y_test, y_pred):
+      
+    print("Confusion Matrix: ",
+        confusion_matrix(y_test, y_pred))
+      
+    print ("Accuracy : ",
+    accuracy_score(y_test,y_pred)*100)
+      
+    print("Report : ",
+    classification_report(y_test, y_pred))
+  
+# Driver code
+def main():
+      
+    # Building Phase
+    data = importdata()
+    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    clf_gini = train_using_gini(X_train, X_test, y_train)
+    clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
+      
+    # Operational Phase
+    print("Results Using Gini Index:")
+      
+    # Prediction using gini
+    y_pred_gini = prediction(X_test, clf_gini)
+    cal_accuracy(y_test, y_pred_gini)
+      
+    print("Results Using Entropy:")
+    # Prediction using entropy
+    y_pred_entropy = prediction(X_test, clf_entropy)
+    cal_accuracy(y_test, y_pred_entropy)
+      
+      
+# Calling main function
+if __name__=="__main__":
+    main()
