8b0e5e815c8a4699cfd4bfe22c5ec75cd4230c52
MagdalenaKumala <79920505+MagdalenaKumala@users.noreply.github.com>
2021-04-13 19:44:54 +0200

    Update drzewa_binarne.py

--- drzewa_binarne.py
+++ drzewa_binarne.py
@@ -15,3 +15,61 @@ df = pd.read_csv('drzewko_dane.csv')
 df.drop('Unnamed: 0', axis = 1, inplace = True)
 
 print(cl(df.head(), attrs = ['bold']))
+
+df.info()
+
+for i in df.Sex.values:
+    if i  == 'M':
+        df.Sex.replace(i, 0, inplace = True)
+    else:
+        df.Sex.replace(i, 1, inplace = True)
+
+for i in df.BP.values:
+    if i == 'LOW':
+        df.BP.replace(i, 0, inplace = True)
+    elif i == 'NORMAL':
+        df.BP.replace(i, 1, inplace = True)
+    elif i == 'HIGH':
+        df.BP.replace(i, 2, inplace = True)
+
+for i in df.Cholesterol.values:
+    if i == 'LOW':
+        df.Cholesterol.replace(i, 0, inplace = True)
+    else:
+        df.Cholesterol.replace(i, 1, inplace = True)
+
+print(cl(df, attrs = ['bold']))
+
+
+X_var = df[['Sex', 'BP', 'Age', 'Cholesterol', 'Na_to_K']].values # independent variable
+y_var = df['Drug'].values # dependent variable
+
+print(cl('X variable samples : {}'.format(X_var[:5]), attrs = ['bold']))
+print(cl('Y variable samples : {}'.format(y_var[:5]), attrs = ['bold']))
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.2, random_state = 0)
+
+print(cl('X_train shape : {}'.format(X_train.shape), attrs = ['bold'], color = 'black'))
+print(cl('X_test shape : {}'.format(X_test.shape), attrs = ['bold'], color = 'black'))
+print(cl('y_train shape : {}'.format(y_train.shape), attrs = ['bold'], color = 'black'))
+print(cl('y_test shape : {}'.format(y_test.shape), attrs = ['bold'], color = 'black'))
+
+model = dtc(criterion = 'entropy', max_depth = 4)
+model.fit(X_train, y_train)
+
+pred_model = model.predict(X_test)
+
+print(cl('Accuracy of the model is {:.0%}'.format(accuracy_score(y_test, pred_model)), attrs = ['bold']))
+
+
+feature_names = df.columns[:5]
+target_names = df['Drug'].unique().tolist()
+
+plot_tree(model, 
+          feature_names = feature_names, 
+          class_names = target_names, 
+          filled = True, 
+          rounded = True)
+
+plt.savefig('tree_visualization.png') 
